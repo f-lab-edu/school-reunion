8984e2eb03d7ced80b9778500af04d62b01c3e35
BottleMoon <gwonbj.dev@gmail.com>
2024-04-10 15:07:14 +0900

    feat: api gateway with jwt vaildate

--- board/src/main/java/flab/schoolreunion/board/repository/ReunionRepository.java
+++
@@ -1,9 +0,0 @@
-package flab.schoolreunion.board.repository;
-
-import flab.schoolreunion.board.entity.Reunion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ReunionRepository extends JpaRepository<Reunion, Long> {
-}
